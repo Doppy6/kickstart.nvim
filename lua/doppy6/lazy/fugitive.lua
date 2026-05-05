f6ca19a74d32a18879a65a526a276935d5c1fa75
Doppy6 <obaidar.omar@gmail.com>
2026-05-05 15:31:26 +0700

    set push and pull keymaps

--- lua/doppy6/lazy/fugitive.lua
+++ lua/doppy6/lazy/fugitive.lua
@@ -10,7 +10,8 @@ return {
         if vim.bo.ft ~= 'fugitive' then return end
 
         local bufnr = vim.api.nvim_get_current_buf()
-        vim.keymap.set('n', '<leader>P', function() vim.cmd.Git('push', { desc = 'Push to Repo', buffer = bufnr, remap = false }) end)
+        vim.keymap.set('n', '<leader>p', function() vim.cmd.Git('push', { desc = 'Push to Repo', buffer = bufnr, remap = false }) end)
+        vim.keymap.set('n', '<leader>P', function() vim.cmd.Git({ 'pull', '--rebase' }, { desc = 'Pulling from Repo', buffer = bufnr, remap = false }) end)
       end,
     })
   end,
