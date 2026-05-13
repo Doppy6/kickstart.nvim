ada1745ab96c62ded1152c24643ea5d619fe2b68
Doppy6 <obaidar.omar@gmail.com>
2026-05-13 13:58:01 +0700

    fix: debugpy dap

--- lua/doppy6/lazy/debug.lua
+++ lua/doppy6/lazy/debug.lua
@@ -155,7 +155,7 @@ return {
       else
         cb {
           type = 'executable',
-          command = 'path/to/virtualenvs/debugpy/bin/python',
+          command = os.getenv 'HOME' .. '/.venv/debugpy/bin/python',
           args = { '-m', 'debugpy.adapter' },
           options = {
             source_filetype = 'python',
@@ -163,30 +163,29 @@ return {
         }
       end
     end
-
     dap.configurations.python = {
       {
-        type = 'python',
+        -- The first three options are required by nvim-dap
+        type = 'python', -- the type here established the link to the adapter definition: `dap.adapters.python`
         request = 'launch',
-        name = 'launch file',
-        program = '${file}',
+        name = 'Launch file',
+
+        -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+        program = '${file}', -- This configuration will launch the current file if used.
         pythonPath = function()
-          local cwd = vim.fn.getcwd()
-          if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
-            return cwd .. '/venv/bin/python'
-          elseif vim.fn.executable(cwd .. '/.venv/bin/python') then
-            return cwd .. '/.venv/bin/python'
-          else
-            return '/usr/bin/python3'
-          end
+          -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+          -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+          -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+          if os.getenv 'VIRTUAL_ENV' ~= '/usr' then return os.getenv 'VIRTUAL_ENV' .. '/bin/python' end
+          return os.getenv 'VIRTUAL_ENV' .. '/bin/python3'
         end,
       },
     }
-
     dap.adapters.gdb = {
       type = 'executable',
       command = 'gdb',
-      args = { '--interpreter = dap', '--eval-command', 'set print pretty on' },
+      args = { '--interpreter=dap', '--eval-command', 'set print pretty on' },
     }
 
     dap.configurations.c = {
@@ -194,11 +193,11 @@ return {
         name = 'Launch',
         type = 'gdb',
         request = 'launch',
-        program = function() return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file') end,
-        args = {},
-        cwd = '${workspaceFolder',
+        program = function() return vim.fn.input('Path to executable: ', vim.uv.cwd() .. '/', 'file') end,
+        args = {}, -- provide arguments if needed
+        cwd = '${workspaceFolder}',
+        stopAtBeginningOfMainSubprogram = false,
       },
-      stopAtBeginningOfMainSubprogram = false,
       {
         name = 'Select and attach to process',
         type = 'gdb',
